27f941b62f58f74c75326170c2e9ae6f44575b6a
MrMoebius <e.spadaro.ro@gmail.com>
2026-01-29 19:16:29 +0100

    feat: Aniadir inicializador de datos para el usuario administrador predeterminado y los roles

--- src/main/java/org/davide/ggbproyect/security/SecurityConfig.java
+++ src/main/java/org/davide/ggbproyect/security/SecurityConfig.java
@@ -46,12 +46,16 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll() // Allow H2 Console
                                 .anyRequest().authenticated()
                 ).exceptionHandling(exception -> exception
                         .authenticationEntryPoint(authenticationEntryPoint)
                 ).sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
+        
+        // Allow H2 Console frames
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
 
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
